1c4b2d36927ef3db1f5d3bcf8015aba85eaa4729
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2024-03-08 16:25:46 +0530

    added_auth_middleware_and_user_routes

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken')
+
+
+
+
+
+const authenticatingUser = (req, res, next) => {
+  // Get the token from the request headers
+  const token = req.header('x-auth-token');
+
+  // Check if token is present
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.SECRET_KEY); // Replace 'your-secret-key' with your actual secret key
+
+    // Attach the user from the token to the request object
+    req.user = decoded.user;
+
+    // Continue with the next middleware or route handler
+    next();
+  } catch (err) {
+    // Handle invalid token
+    console.error(err);
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
+
+
+
+
+module.exports = {
+    authenticatingUser
+}
---
+++ routes/user.routes.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const{ registerUser,loginUser,profile,workouts,fetchingWorkout } = require('../controllers/user.controllers.js')
+const{ editingWorkouts,deletingWorkout } = require('../controllers/crud.controllers.js')
+const { authenticatingUser } = require('../middlewares/auth.middlewares.js')
+
+
+
+const router = Router()
+
+router.route('/register-user').post(registerUser)
+router.route('/login-user').post(loginUser)
+router.route('/profile/:id').get(authenticatingUser,profile)
+router.route('/workouts').post(authenticatingUser,workouts)
+router.route('/fetch-workout/:id').get(authenticatingUser,fetchingWorkout)
+router.route('/delete-workout/:id').delete(authenticatingUser,deletingWorkout)
+router.route('/update-workout/:id').delete(authenticatingUser,editingWorkouts)
+
+module.exports = router
